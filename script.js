c09a9825024e1c742dcde57545f5550b45fed799
dipsthedestroya <dipupurecha01@gmail.com>
2021-09-30 13:21:32 +0400

    cleaned code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function multiply(a,b){
 
 function divide(a,b){
     if(b==0)
-        return("Error");
+        return("Error, cannot divide by 0");
     return (a/b);
 }
 function clear(){
@@ -22,93 +22,119 @@ function clear(){
     isLastButtonOperator=0;
 }
 
-function displayNumber(button){
-        if(paraBottom.textContent=="0" || isLastButtonOperator || isLastButtonEqualTo)
-            paraBottom.textContent=button.textContent
-        else if(!isLastButtonOperator)
-            paraBottom.textContent+=button.textContent;
-
-        total=paraBottom.textContent; //change value of total to current current number
-        isLastButtonOperator=0;
-        isLastButtonEqualTo=0;
-    } 
-
-//computes the total of the two arguments passed 
-function computeTotal(a,b,button){
- 
-        if(operator=="+")
-            total=add(a,b);
-        else if(operator=="-")
-            total=subtract(a,b);
-        else if(operator=="*")
-            total=multiply(a,b);
-        else if(operator=="÷"){
-            total=divide(a,b);
-            if(total=="Error")
-                alert("Cannot divide by 0");          
-        }
-  
+function calcTotal(a,b,operator){
+    if(operator=="+")
+        return add(a,b);
+    else if(operator=="-")
+        return subtract(a,b);
+    else if(operator=="*")
+        return multiply(a,b);
+    else 
+        return divide(a,b);
 }
 
-function checkDividebyZero(){
-    if(total=="Error")
-    paraBottom.textContent="";
+function updateScreen(value,operator){
+    paraTop.textContent=`${value} ${operator}`;
+    paraBottom.textContent=`${value}`;
 }
 
-function displayTotal(a,b,button){
-    checkDividebyZero();
-    if(button.textContent !="=") //if button is not '=' then the display the total(which doesn't change) and operator(which could change) on the top screen
-        paraTop.textContent=`${total} ${button.textContent}`;
-    else if( button.textContent=="="){ //if button is '=' and the last button was a number then condition is true
-        paraTop.textContent=`${a} ${operator} ${b} =`;
-    }
-    paraBottom.textContent=total;
-}
 
 const bottomScreen=document.querySelector('.bottom');
-const topScreen=document.querySelector('.top')
-const buttons=document.querySelectorAll('button');
-let isLastButtonOperator=0;
-let isLastButtonEqualTo=0;
+const topScreen=document.querySelector('.top');
 let paraBottom=document.createElement('p');
 let paraTop=document.createElement('p');
-let operator;
-let total=0;
+bottomScreen.appendChild(paraBottom);
+topScreen.appendChild(paraTop);
+paraBottom.textContent="0";
+let prevOperator,curOperator,prevButton;
+let buttonClass,buttonText;
+let decimalUsed=0;
 
-paraBottom.textContent=0;
-bottomScreen.append(paraBottom);
-topScreen.append(paraTop);
-
+const buttons=document.querySelectorAll('button');
 
 buttons.forEach((button)=>{
+    button.addEventListener('click',()=>{    
+           buttonClass=button.parentElement.classList;
+        buttonText=button.textContent;
 
-    button.addEventListener('click',()=>{
-        if(button.parentElement.classList=="numbers")
-            displayNumber(button);
-        else if(button.classList=="operator"){
-            if(!paraTop.textContent)//if there is no operator pressed then the number previously pressed along with the operator will be displayed on the top half of the screen
-                paraTop.textContent=`${paraBottom.textContent} ${button.textContent}`;
-            else if(paraTop.textContent && paraBottom.textContent){//if there exists text on the top half of the calculator 
-                let a=parseFloat(paraTop.textContent.replace(/[^0-9.-]/g,''));
-                let b=parseFloat(paraBottom.textContent.replace(/[^0-9.-]/g,''));
-                if(isLastButtonOperator==0){//if last button pressed is not operator then total is computed
-                    computeTotal(a,b); 
-                    displayTotal(a,b,button);
-                }
-                else if(button.textContent!="=" && isLastButtonOperator){//last button pressed is not '=' but the last button is an operator, then we only change the
-                    paraTop.textContent=`${total} ${button.textContent}`;
-                }
+        if(buttonClass=="numbers"){ 
+            if(paraBottom.textContent.indexOf('.')==-1)
+                decimalUsed=0;
+            else
+                decimalUsed=1;
 
+            if(prevButton!="numbers" && button.textContent!="."){
+                paraBottom.textContent=buttonText;
             }
-            if(button.textContent!="=")//since = is not an operator
-                operator=button.textContent;
-            isLastButtonOperator=1;
+            else if(decimalUsed==0 || button.textContent!="."){
+                paraBottom.textContent+=buttonText;
+            }
+            prevButton="numbers";
         }
+
+        else if(button.classList=="operator"){
+            curOperator=button.textContent;
+            console.log(curOperator);
+
+            if(curOperator=="%"){
+                let a=parseFloat(paraBottom.textContent);
+                b=100;
+                let total=calcTotal(a,b,divide);
+                paraBottom.textContent=total;
+                prevOperator="%";
+            }
+            else if(curOperator!="="){
+                //if the previous button is number or there is no previous button, then 0(paraBottom.textContent) along with the operator are displayed on the top screen 
+                if(!prevOperator){
+                    updateScreen(paraBottom.textContent,curOperator);
+                }
+            
+                else if((prevButton=="numbers" || prevButton=="backspace"||prevOperator=="%" ) && paraTop.textContent && prevOperator!="="){
+                    let a,b,total;
+                    a=parseFloat(paraTop.textContent.replace(/[^0-9.-]/g));
+                    b=parseFloat(paraBottom.textContent.replace(/[^0-9.-]/g));
+                    if(prevOperator!="%")
+                        total=calcTotal(a,b,prevOperator);
+                    else 
+                        total=calcTotal(a,b,curOperator);
+                    updateScreen(total,curOperator);
+                }
+
+                else{
+                    updateScreen(paraBottom.textContent,curOperator);
+                }
+                prevOperator=curOperator;
+            }   
+
+            else if(curOperator=="="){
+                if(prevButton && prevButton!="operator" && prevButton!="backspace"){
+                    let a=parseFloat(paraTop.textContent.replace(/[^0-9.-]/g));
+                    let b=parseFloat(paraBottom.textContent.replace(/[^0-9.-]/g));
+                    let total=calcTotal(a,b,prevOperator);
+                    paraTop.textContent=`${a} ${prevOperator} ${b} =`
+                    paraBottom.textContent=`${total}`;
+                    prevOperator="=";
+                }
+            }
+            prevButton="operator";
+        }
+        
         else if(button.classList=="clear"){
-            clear(button);
+            paraBottom.textContent="0"
+            paraTop.textContent="";
+            curOperator="";
+            prevOperator="";
+            prevButton="clear";
         }
+
         else if(button.classList=="backspace"){
             paraBottom.textContent=paraBottom.textContent.slice(0,-1);
+            if(!paraBottom.textContent)
+                paraBottom.textContent=0;
+            prevButton="backspace";
         }
+
     })
+
 })
+
